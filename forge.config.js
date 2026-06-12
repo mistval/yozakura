20a67c2e2bbe2fd5f53e133ef6069a9ae901fdeb
mistval <randallschmidt1@gmail.com>
2026-06-11 20:29:07 -0700

    get rid of squirrel

--- forge.config.js
+++ forge.config.js
@@ -22,10 +22,6 @@ module.exports = {
   rebuildConfig: {},
   makers: [
     {
-      name: '@electron-forge/maker-squirrel',
-      config: {},
-    },
-    {
       name: '@electron-forge/maker-zip',
       platforms: ['win32', 'darwin', 'linux'],
     },
